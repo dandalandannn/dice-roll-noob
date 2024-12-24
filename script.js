48befe3a322ffd54d27772848d0a8392bc80b20d
dandalandannn <danielgwen.domingo01@gmail.com>
2024-12-24 14:58:03 +0800

    wow may pic na

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const ROLL = document.getElementById("ROLL");
-const rollContainer = document.getElementById("rollContainer");
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const ROLLED = document.getElementById("ROLLED");
+function rollDice(){
+    const amountOfDice = document.getElementById("amountOfDice").value;
+    const button = document.getElementById("button");
+    const result = document.getElementById("result");
+    const resultImages = document.getElementById("resultImages");
+    const total = document.getElementById("total");
 
-
-let roll1;
-let roll2;
-let total;
-
-ROLL.onclick = function(){
-    rollContainer.style.display = 'block';
-    roll1 = Math.floor(Math.random() * 6 ) + 1;
-    roll2 = Math.floor(Math.random() * 6 ) + 1;
-    num1.textContent = roll1;
-    num2.textContent = roll2;
-    total = roll1+roll2;
-    if (total == 12){
-        ROLLED.textContent = `Congrats u won!`;
-    }else{
-        ROLLED.textContent = `You rolled a total of ${total}!`;
+    if (Number(amountOfDice)>10 || Number(amountOfDice)<1){
+        result.textContent = `Error!\nEnter from 1-10`;
+        resultImages.textContent = "";
+        total.textContent = "";
+        return;
     }
+
+    const values = [];
+    const images = [];
+    let temp = 0;
+
+    for (let i = 0; i<amountOfDice; i++){
+        const value = Math.floor(Math.random()* 6 ) +1;
+        values.push(value);
+        images.push(`<img src="images/${value}.png">`);
+        temp += value;
+    }
+    result.textContent = `Rolled: ${values.join(`, `)}`;
+    resultImages.innerHTML = images.join(` `);
+    total.textContent = `Total: ${temp}`;
 }
